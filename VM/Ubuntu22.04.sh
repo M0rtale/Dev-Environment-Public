3e917b02c13ef057e06ac133e41a5fdd46ae8806
M0rtale <pro.juda@gmail.com>
2024-09-17 01:50:33 -0400

    added first script

---
+++ VM/Ubuntu22.04.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo apt install vim gdb wget git curl zsh python3 python3-pip libssl-dev -y
+# install pip & ipython 
+sudo apt-get install ipython3 -y
+sudo apt-get install gcc-multilib -y
+# install pwntools
+pip3 install pwntools
+# install on-my-zsh   
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# install peda pwngdb
+cd 
+git clone https://github.com/longld/peda.git ~/peda
+git clone https://github.com/scwuaptx/Pwngdb.git
+cp ~/Pwngdb/.gdbinit ~/
+# install one_gadget
+sudo apt install gem ruby ruby-dev -y
+sudo gem install one_gadget
+sudo gem install seccomp-tools
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i '/^plugins=(/ s/$/ zsh-autosuggestions/' ~/.zshrc && echo "" >> ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i '/^plugins=(/ s/$/ zsh-syntax-highlighting/' ~/.zshrc && echo "" >> ~/.zshrc
+
